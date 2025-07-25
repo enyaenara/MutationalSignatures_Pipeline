2ffc6daf68323c0603b6995e2bdc0360cfd361af
enyaenara <enya_mar@hotmail.com>
2025-07-25 14:49:19 -0600

    Recomendaciones para el proyecto

---
+++ Project_Data/README.sh
@@ -0,0 +1,6 @@
+#En esta carpeta se recomienda crear una tabla de datos de relevancia respecto
+# a las secuencias analizadas
+#En esta tabla se pueden crear tantas columnas como sea necesario (a criterio del usuario)
+#Por ejemplo: Identificador, nombre de la muestra, plataforma, librería, etc.
+#De esta manera la selección de readgroups será más sencilla.
+
