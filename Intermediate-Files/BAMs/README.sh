ff43bfc84c114647f9d4ea99633b6c1cb7332eaf
enyaenara <enya_mar@hotmail.com>
2025-07-25 14:26:18 -0600

    Descripción de la carpeta BAMs

---
+++ Intermediate-Files/BAMs/README.sh
@@ -0,0 +1,9 @@
+#Esta carpeta contiene todos los archivos en formato BAM 
+#obtenidos de los Scripts 06-09
+#Este formato es lo mismo que el formato SAM (Sequence Alignment Map) pero en binario
+#Como se observa en los Scripts mencionados
+#Se realiza el alineamiento de secuencias y se obtiene el merged.
+#Se sigue el mismo principio para el cambio de las variables 
+#como en el Script 02 de Trimmomatic
+#El cambio más importante es el comando para llamar el programa correspondiente
+#las opciones y los argumentos que cada uno necesita.
